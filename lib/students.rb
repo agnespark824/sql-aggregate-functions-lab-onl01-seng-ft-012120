e03fcd451f58163bd81165b507b953c20c24cbc3
Agnes Park <agnespark824@gmail.com>
2020-02-19 20:32:34 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,22 +1,22 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks. 
 
 def highest_student_gpa
-  "SELECT MAX(gpa) FROM memory;"
+  "SELECT MAX(gpa) FROM sql_runner;"
 end
 
 def lowest_student_gpa
-  "SELECT MIN(gpa) FROM memory;"
+  "SELECT MIN(gpa) FROM sql_runner;"
 end
 
 def average_student_gpa
-  "SELECT AVG(gpa) FROM memory;"
+  "SELECT AVG(gpa) FROM sql_runner;"
 end
 
 def total_tardies_for_all_students
-  "SELECT COUNT(tardies) FROM memory;"
+  "SELECT COUNT(tardies) FROM sql_runner;"
 end
 
 def average_gpa_for_9th_grade
-  "SELECT AVG(gpa) FROM memory WHERE grade = 11;"
+  "SELECT AVG(gpa) FROM sql_runner WHERE grade = 11;"
 end
 
