ab7377fa10fa428a6f679019d5f0425fb674a796
Agnes Park <agnespark824@gmail.com>
2020-02-19 20:28:51 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT TOTAL(tardies) FROM memory;"
+  "SELECT COUNT(tardies) FROM memory;"
 end
 
 def average_gpa_for_9th_grade
